f467cc1d3345b3a1a9f80946dddeb13d597e57cb
ODIER Jerome <jerome.odier@lpsc.in2p3.fr>
2024-05-19 20:44:12 +0200

    update

--- src/index.js
+++ src/index.js
@@ -9,14 +9,14 @@ import mqttPlugin from './plugins/mqtt';
 
 import useIndiStore from './stores/indi';
 
+import NavTabs from './components/controls/NavTabs.vue';
+import TabPane from './components/controls/TabPane.vue'
+
 import IndiDevices from './components/IndiDevices.vue';
 import IndiTopology from './components/IndiTopology.vue';
 import IndiVariables from './components/IndiVariables.vue';
 import IndiDashboard from './components/IndiDashboard.vue';
 import IndiMonitoring from './components/IndiMonitoring.vue'
-
-import NavTabs from './components/controls/NavTabs.vue';
-import TabPane from './components/controls/TabPane.vue'
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 
@@ -27,6 +27,12 @@ const setup = (app) => {
 
     app.component('NavTabs', NavTabs);
     app.component('TabPane', TabPane);
+
+    app.component('IndiDevices', IndiDevices);
+    app.component('IndiTopology', IndiTopology);
+    app.component('IndiVariables', IndiVariables);
+    app.component('IndiDashboard', IndiDashboard);
+    app.component('IndiMonitoring', IndiMonitoring);
 };
 
 /*--------------------------------------------------------------------------------------------------------------------*/
@@ -34,11 +40,6 @@ const setup = (app) => {
 export {
     setup,
     useIndiStore,
-    IndiDevices,
-    IndiTopology,
-    IndiVariables,
-    IndiDashboard,
-    IndiMonitoring
 };
 
 /*--------------------------------------------------------------------------------------------------------------------*/
