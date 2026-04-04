176f6676c8b0befa7bb46bd5f3546697356b205c
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2026-04-04 21:02:27 +0200

    update

--- src/components/dashboard/NyxHome.vue
+++ src/components/dashboard/NyxHome.vue
@@ -7,6 +7,10 @@ import useNyxStore from '../../stores/nyx';
 /* VARIABLES                                                                                                          */
 /*--------------------------------------------------------------------------------------------------------------------*/
 
+const gitRelease = globalThis.__GIT_RELEASE__;
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
 const nyxStore = useNyxStore();
 
 /*--------------------------------------------------------------------------------------------------------------------*/
@@ -47,8 +51,14 @@ const emit = defineEmits([
 
     <!-- *********************************************************************************************************** -->
 
-    <div class="position-absolute opacity-50" style="bottom: 4px; font-size: 0.9rem;">
+    <div class="position-absolute opacity-50 bottom-0" style="font-size: 0.9rem;">
+
         Copyright © 2024-{{ new Date().getFullYear() }} <a href="https://nyxlib.org/" target="_blank">Nyx Lab</a> — <a href="https://odier.xyz/" target="_blank">Jérôme ODIER</a>, <a href="https://lpsc.in2p3.fr/" target="_blank">LPSC</a> / <a href="https://cnrs.fr/" target="_blank">CNRS</a>.
+
+        <span v-if="gitRelease">
+            GIT commit: <a :href="`https://github.com/nyxlib/nyx-lab/tree/${gitRelease.gitCommit}`" target="_blank">{{ gitRelease.gitBranch }} / {{ gitRelease.gitCommit }}</a>, build date: {{ gitRelease.date?.split('T')[0] }}.
+        </span>
+
     </div>
 
     <!-- *********************************************************************************************************** -->
