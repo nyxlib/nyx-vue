e6d58df2130261fa6b139d7ee52d8f5c19cd66ff
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2026-04-16 11:46:42 +0200

    update

--- src/components/dashboard/NyxHome.vue
+++ src/components/dashboard/NyxHome.vue
@@ -25,7 +25,7 @@ const emit = defineEmits([
 
     <div class="text-center" style="width: 50%; max-width: 225px;">
 
-        <svg xmlns="http://www.w3.org/2000/svg" :fill="nyxStore.isConnected ? '#198754' : '#6c757d'" viewBox="0 0 16 16" style="width: 90%; height: auto;">
+        <svg xmlns="http://www.w3.org/2000/svg" :fill="nyxStore.isConnected ? '#198754' : '#6c757d'" viewBox="0 0 16 16" style="width: 100%; height: auto;">
             <path d="M7.5 1v7h1V1z" /><path d="M3 8.812a5 5 0 0 1 2.578-4.375l-.485-.874A6 6 0 1 0 11 3.616l-.501.865A5 5 0 1 1 3 8.812" />
         </svg>
 
